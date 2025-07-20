3b8f1e5786cddeabfb06ab09487a8727bc122cb6
joobumjin <bumjin.joo@gmail.com>
2025-07-20 18:11:04 -0400

    try arrow change

--- src/app/components/homeButton.tsx
+++ src/app/components/homeButton.tsx
@@ -11,7 +11,7 @@ export default function HomeButton() {
                 className={[hbStyles.left, styles.card].join(' ')}
                 rel="noopener noreferrer"
             >
-                <p><span className={styles.toLeft}>&lt;</span> Back To Home</p>
+                <p><span className={[styles.toLeft, hbStyles.arrow].join(' ')}>&lt;</span> Back To Home</p>
             </Link>
         </span>
     );
