46466374b2bb9171cb70abfe37a622d1852ebb96
joobumjin <bumjin.joo@gmail.com>
2025-08-01 01:08:13 -0400

    feat: new daebak img, change video

--- src/app/daebak/page.tsx
+++ src/app/daebak/page.tsx
@@ -122,7 +122,7 @@ export default function DAEBAK() {
                 </p>
 
                 <Video 
-                    vid_src="https://www.youtube.com/embed/XK6OhebFJ5k"
+                    vid_src="https://www.youtube.com/embed/gGvDaXbnFbY"
                     vid_alt="asdf"
                 />
             </span>  
